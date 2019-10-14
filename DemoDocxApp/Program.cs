ccf87dcc3e7efb1b3c6e6187e2929159901378af
DESKTOP-VJEDKLD\Владимир <vklopkov@gmail.com>
2019-10-15 00:11:28 +0300

    programm add Fillrow

--- DemoDocxApp/Program.cs
+++ DemoDocxApp/Program.cs
@@ -9,6 +9,26 @@ namespace DemoDocxApp
 {
     class Program
     {
+        static void FillRow(Row row, string[] val,System.Drawing.Color color,bool header = false)
+        {
+            int i = 0;
+            foreach (Cell cell in row.Cells)
+            {
+                if (header)
+                {
+                    cell.Paragraphs.First().Append(val[i]).Bold();
+                    cell.FillColor = color;
+                    i++;
+                }
+                else
+                {
+                    cell.Paragraphs.First().Append(val[i]);
+                    if (i==1) cell.FillColor = color;
+                    i++;
+                }
+
+            }
+        }
         static void Main(string[] args)
         {
             string fileName = @"C:\temp\exempleWord.docx";
@@ -42,36 +62,47 @@ namespace DemoDocxApp
               */
 
 
-            //Create Table with 2 rows and 4 columns. 
+            //Create Table with 2 rows and 3 columns. 
+            var header = new string[] { "Было", "Статус", "Стало" };
+            Table t = doc.AddTable(2, header.Length);
 
             Image img = doc.AddImage(@"C:\Users\Владимир\source\repos\DemoDocxApp\Image.PNG");
             Picture pic = img.CreatePicture();
-            Table t = doc.AddTable(2, 3);
-            Table t2 = doc.AddTable(2, 3);
+            
+            
             //t.AutoFit = AutoFit.Fixed;
             t.SetWidthsPercentage(new float[] { 45F,10F, 45F },doc.PageWidth);
             t.Alignment = Alignment.center;
             t.Design = TableDesign.TableGrid;
             //Fill  by adding text.  
-            t.Rows[0].Cells[0].Paragraphs.First().Append("Было").Bold();
-            t.Rows[0].Cells[1].Paragraphs.First().Append("Статус").Bold();
-            t.Rows[0].Cells[2].Paragraphs.First().Append("Стало").Bold();
-            t.Rows[0].Cells[0].FillColor = System.Drawing.Color.LightGray;
-            t.Rows[0].Cells[1].FillColor = System.Drawing.Color.LightGray;
-            t.Rows[0].Cells[2].FillColor = System.Drawing.Color.LightGray;
+            //t.Rows[0].Cells[0].Paragraphs.First().Append("Было").Bold();
+            //t.Rows[0].Cells[1].Paragraphs.First().Append("Статус").Bold();
+            //t.Rows[0].Cells[2].Paragraphs.First().Append("Стало").Bold();
+            //t.Rows[0].Cells[0].FillColor = System.Drawing.Color.LightGray;
+            //t.Rows[0].Cells[1].FillColor = System.Drawing.Color.LightGray;
+            //t.Rows[0].Cells[2].FillColor = System.Drawing.Color.LightGray;
+            //foreach (Cell cell in t.Rows[0].Cells)
+            //{ 
+            //    cell.FillColor = System.Drawing.Color.LightGray;
+            //}
+            
+            FillRow(t.Rows[0], header, System.Drawing.Color.LightGray,true);
 
-            t.Rows[1].Cells[0].Paragraphs.First().Append("Отчет должен строится на основе данных, получаемых из следующих систем и подсистем/программных модулей:\n1.ИСУП = MS Project\n2.IBM Лотус, используемые подсистемы:\na.ПМ Поручения\nb.ПМ Согласование ПГ\nc.ПМ Согласования ТЗ\nd.ПМ Договоры\n"
-                , textParagraphFormat).AppendPicture(pic);
-            t.Rows[1].Cells[1].Paragraphs.First().Append("Изменено");
-            t.Rows[1].Cells[1].FillColor = System.Drawing.Color.Yellow;
-            t.Rows[1].Cells[2].Paragraphs.First().Append("Отчет должен строится на основе данных, получаемых из следующих систем и подсистем/программных модулей:\n1.ИСУП = MS Project\n2.IBM Лотус, используемые подсистемы:\na.ПМ Поручения\nb.ПМ Согласование ПГ"
-                , textParagraphFormat).AppendPicture(pic);
+            var r1 = new string[] {
+                "Отчет должен строится на основе данных, получаемых из следующих систем и подсистем/программных модулей:\n1.ИСУП = MS Project\n2.IBM Лотус, используемые подсистемы:\na.ПМ Поручения\nb.ПМ Согласование ПГ\nc.ПМ Согласования ТЗ\nd.ПМ Договоры\n",
+                "Изменено",
+                "Отчет должен строится на основе данных, получаемых из следующих систем и подсистем/программных модулей:\n1.ИСУП = MS Project\n2.IBM Лотус, используемые подсистемы:\na.ПМ Поручения\nb.ПМ Согласование ПГ"
+            };
+            t.InsertRow();
+            FillRow(t.Rows[1], r1, System.Drawing.Color.Yellow);
             t.InsertRow();
             t.Rows[2].Cells[0].Paragraphs.First().AppendPicture(pic);
             t.Rows[2].Cells[1].Paragraphs.First().Append("Удалено");
             t.Rows[2].Cells[1].FillColor = System.Drawing.Color.Red;
             
             t.InsertRow();
+
+            //FillRow(t.Rows[3], r3, System.Drawing.Color.LightGreen);
             t.Rows[3].Cells[2].Paragraphs.First().Append("Отчет должен поддерживать фильтрацию (отображение) целевых задач по следующим условиям, с учётом пересчета кол-ва задач, описанного выше:\n1.По профильной системе\n2.По категории\n3.По дате / периоду\n4.По статусу задачи(завершенна, выполняется, планируется)")
                 .InsertTableAfterSelf(t);
             t.Rows[3].Cells[2].InsertParagraph(doc.InsertParagraph(""));
